177f1bc8bd99e906ff87c752752a81565c12ae5e
nataliayepes11 <nataliayepes11@gmail.com>
2026-01-21 12:41:40 -0300

    FUNCTION + WHILE + MATH + FLOOR + IF/ELSE IF/ELSE

--- Ej8.js
+++ Ej8.js
@@ -2,7 +2,7 @@
 Para practicar la implementación del algoritmo de búsqueda binaria, vamos a empezar con un ejemplo simple. 
 Quiero que se familiaricen con la sintaxis del mismo, por lo que no hare énfasis en los datos, sino en la solución.
 Dada la siguiente lista:
-AGREGAR IMAGEN*************
+let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 Utilizar el algoritmo de búsqueda binaria para responder los siguientes ítems:
 o ¿Cuál es la posición del número 1?
 o ¿Cuál es la posición del número 5?
@@ -14,3 +14,30 @@ un cálculo visual la posición de cada elemento, pero, la propuesta que les hag
 que “busque” ese número, por ejemplo, el 6, y nos indique por consola la posición del mismo.
 El objetivo de este ejercicio es que puedan practicar la sintaxis sin añadir complejidad extra.
 */
+
+let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+function busquedaBinaria(list, objetivo) {
+    let inicio = 0;
+    let fin = list.length - 1;
+
+    while (inicio <= fin) {
+        let medio = Math.floor((inicio + fin) / 2);
+
+        if (list[medio] === objetivo) {
+            return medio;
+        } else if (list[medio] < objetivo) {
+            inicio = medio + 1;
+        } else {
+            fin = medio - 1;
+        }
+    }
+
+    return -1;
+}
+
+console.log("Posición del 1:", busquedaBinaria(list, 1))
+console.log("Posición del 5:", busquedaBinaria(list, 5))
+console.log("Posición del 6:", busquedaBinaria(list, 6))
+console.log("Posición del 9:", busquedaBinaria(list, 9))
+console.log("Posición del 11:", busquedaBinaria(list, 11))
